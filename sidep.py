c480c79de5d3170573ee812665e7a7c01b786b0a
Dan van der Ster <dan@vanderster.com>
2020-07-15 11:56:30 +0200

    sidep.py: print date last updated

--- sidep.py
+++ sidep.py
@@ -5,6 +5,7 @@
 
 import plotly.express as px
 import pandas
+from time import strftime, localtime
 
 regions = {
   1: 'Guadeloupe',
@@ -30,17 +31,24 @@ regions = {
   94: 'Corse',
 }
 
+now = strftime("%Y-%m-%d %H:%M", localtime())
+
 # Regions
 data = pandas.read_csv('https://www.data.gouv.fr/fr/datasets/r/001aca18-df6a-45c8-89e6-f82d689e6c01', sep=';')
 data['reg'].replace(regions, inplace=True)
 everyone = data[data['cl_age90'] == 0]
 date = everyone.tail(1)['jour'].array[0]
-fig = px.bar(everyone, x='jour', y='P', title='Regional SI-DEP Positive COVID-19 Tests as of %s' % date, template='ggplot2', color='reg', color_discrete_sequence=px.colors.qualitative.Dark24)
+fig = px.bar(everyone, x='jour', y='P', template='ggplot2', color='reg',
+             title='France Regional SI-DEP Positive COVID-19 Tests as of %s<br><sub>Last updated %s</sub>' % (date, now),
+             color_discrete_sequence=px.colors.qualitative.Dark24,
+             labels={'P':'Positive Tests', 'jour':'Date'},)
 fig.write_html(file='index.html')
 
 # All of France
 data = pandas.read_csv('https://www.data.gouv.fr/fr/datasets/r/dd0de5d9-b5a5-4503-930a-7b08dc0adc7c', sep=';')
 everyone = data[data['cl_age90'] == 0]
 date = everyone.tail(1)['jour'].array[0]
-fig = px.bar(everyone, x='jour', y='P', title='France SI-DEP Positive COVID-19 Tests as of %s' % date, template='ggplot2')
+fig = px.bar(everyone, x='jour', y='P', template='ggplot2',
+             title='France SI-DEP Positive COVID-19 Tests as of %s<br><sub>Last updated %s</sub>' % (date, now),
+             labels={'P':'Positive Tests', 'jour':'Date'},)
 fig.write_html(file='national.html')

4909119aee4e303313b0319d3aab3132167f4eb4
Dan van der Ster <daniel.vanderster@cern.ch>
2020-07-23 11:58:59 +0200

    Better subtitle

--- sidep.py
+++ sidep.py
@@ -32,9 +32,11 @@ regions = {
   94: 'Corse',
 }
 
+# subtitle
 os.environ['TZ'] = 'Europe/Paris'
 time.tzset()
 now = time.strftime("%Y-%m-%d %H:%M %Z", time.localtime())
+sub = '<sub>Updated %s | <a href="https://github.com/dvanders/SI-DEP">Code</a> | <a href="https://dvanders.github.io/SI-DEP/national.html" target="_top">National</a> |  <a href="https://dvanders.github.io/SI-DEP/index.html" target="_top">Regional</a> | <a href="https://dvanders.github.io/SI-DEP/dep.html" target="_top">Departmental</a></sub>' % now
 
 # Regions
 data = pandas.read_csv('https://www.data.gouv.fr/fr/datasets/r/001aca18-df6a-45c8-89e6-f82d689e6c01', sep=';')
@@ -42,7 +44,7 @@ data['reg'].replace(regions, inplace=True)
 everyone = data[data['cl_age90'] == 0]
 date = everyone.tail(1)['jour'].array[0]
 fig = px.bar(everyone, x='jour', y='P', template='ggplot2', color='reg',
-             title='France Regional SI-DEP Positive COVID-19 Tests as of %s<br><sub>Updated %s | <a href="https://github.com/dvanders/SI-DEP">Code</a> | <a href="https://dvanders.github.io/SI-DEP/national.html">National</a> |  <a href="https://dvanders.github.io/SI-DEP/index.html">Regional</a> | <a href="https://dvanders.github.io/SI-DEP/dep.html">Departmental</a></sub>' % (date, now),
+             title='France Regional SI-DEP Positive COVID-19 Tests as of %s<br>%s' % (date, sub),
              color_discrete_sequence=px.colors.qualitative.Dark24,
              labels={'P':'Positive Tests', 'jour':'Date'},)
 fig.write_html(file='./build/index.html')
@@ -52,7 +54,7 @@ data = pandas.read_csv('https://www.data.gouv.fr/fr/datasets/r/406c6a23-e283-430
 everyone = data[data['cl_age90'] == 0]
 date = everyone.tail(1)['jour'].array[0]
 fig = px.bar(everyone, x='jour', y='P', template='ggplot2', color='dep',
-             title='France Departmental SI-DEP Positive COVID-19 Tests as of %s<br><sub>Updated %s | <a href="https://github.com/dvanders/SI-DEP">Code</a> | <a href="https://dvanders.github.io/SI-DEP/national.html">National</a> |  <a href="https://dvanders.github.io/SI-DEP/index.html">Regional</a> | <a href="https://dvanders.github.io/SI-DEP/dep.html">Departmental</a></sub>' % (date, now),
+             title='France Departmental SI-DEP Positive COVID-19 Tests as of %s<br>%s' % (date, sub),
              color_discrete_sequence=px.colors.qualitative.Dark24,
              labels={'P':'Positive Tests', 'jour':'Date'},)
 fig.write_html(file='./build/dep.html')
@@ -62,6 +64,6 @@ data = pandas.read_csv('https://www.data.gouv.fr/fr/datasets/r/dd0de5d9-b5a5-450
 everyone = data[data['cl_age90'] == 0]
 date = everyone.tail(1)['jour'].array[0]
 fig = px.bar(everyone, x='jour', y='P', template='ggplot2',
-             title='France SI-DEP Positive COVID-19 Tests as of %s<br><sub>Updated %s | <a href="https://github.com/dvanders/SI-DEP">Code</a> | <a href="https://dvanders.github.io/SI-DEP/national.html">National</a> |  <a href="https://dvanders.github.io/SI-DEP/index.html">Regional</a> | <a href="https://dvanders.github.io/SI-DEP/dep.html">Departmental</a></sub>' % (date, now),
+             title='France National SI-DEP Positive COVID-19 Tests as of %s<br>%s' % (date, sub),
              labels={'P':'Positive Tests', 'jour':'Date'},)
 fig.write_html(file='./build/national.html')

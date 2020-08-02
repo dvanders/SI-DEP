1c00a9bd436974cacaa08812934a2e3ffe3f13d8
Dan van der Ster <daniel.vanderster@cern.ch>
2020-08-02 18:49:55 +0200

    Add 7d Moving Average Charts

--- sidep.py
+++ sidep.py
@@ -36,7 +36,7 @@ regions = {
 os.environ['TZ'] = 'Europe/Paris'
 time.tzset()
 now = time.strftime("%Y-%m-%d %H:%M %Z", time.localtime())
-sub = '<sub>Updated %s | <a href="https://github.com/dvanders/SI-DEP">Code</a> | <a href="https://dvanders.github.io/SI-DEP/national.html" target="_top">National</a> | <a href="https://dvanders.github.io/SI-DEP/index.html" target="_top">Regional</a> | <a href="https://dvanders.github.io/SI-DEP/dep.html" target="_top">Departmental</a></sub>' % now
+sub = '<sub>Updated %s | <a href="https://github.com/dvanders/SI-DEP">Code</a> | <a href="https://dvanders.github.io/SI-DEP/national.html" target="_top">National</a> (<a href="https://dvanders.github.io/SI-DEP/national7.html" target="_top">Weekly</a>) | <a href="https://dvanders.github.io/SI-DEP/index.html" target="_top">Regional</a> (<a href="https://dvanders.github.io/SI-DEP/reg7.html" target="_top">Weekly</a>) | <a href="https://dvanders.github.io/SI-DEP/dep.html" target="_top">Departmental</a> (<a href="https://dvanders.github.io/SI-DEP/dep7.html" target="_top">Weekly</a>)</sub>' % now
 
 # Regions
 data = pandas.read_csv('https://www.data.gouv.fr/fr/datasets/r/001aca18-df6a-45c8-89e6-f82d689e6c01', sep=';')
@@ -49,6 +49,17 @@ fig = px.bar(everyone, x='jour', y='P', template='ggplot2', color='reg',
              labels={'P':'Positive Tests', 'jour':'Date'},)
 fig.write_html(file='./build/index.html')
 
+everyone['P_7'] = everyone.rolling(7).mean()['P'] # hack
+for reg in set(everyone['reg']):
+  _reg = everyone['reg'] == reg
+  everyone['P_7'][_reg] = everyone[_reg].rolling(7).mean()['P']
+
+fig = px.bar(everyone.dropna(), x='jour', y='P_7', template='ggplot2', color='reg',
+             title='France Regional SI-DEP Positive COVID-19 Tests as of %s<br>%s (7d Moving Average)' % (date, sub),
+             color_discrete_sequence=px.colors.qualitative.Dark24,
+             labels={'P':'Positive Tests', 'jour':'Date'},)
+fig.write_html(file='./build/reg7.html')
+
 # Departmental
 data = pandas.read_csv('https://www.data.gouv.fr/fr/datasets/r/406c6a23-e283-4300-9484-54e78c8ae675', sep=';')
 everyone = data[data['cl_age90'] == 0]
@@ -59,6 +70,17 @@ fig = px.bar(everyone, x='jour', y='P', template='ggplot2', color='dep',
              labels={'P':'Positive Tests', 'jour':'Date'},)
 fig.write_html(file='./build/dep.html')
 
+everyone['P_7'] = everyone.rolling(7).mean()['P'] # hack
+for dep in set(everyone['dep']):
+  _dep = everyone['dep'] == dep
+  everyone['P_7'][_dep] = everyone[_dep].rolling(7).mean()['P']
+
+fig = px.bar(everyone.dropna(), x='jour', y='P_7', template='ggplot2', color='dep',
+             title='France Departmental SI-DEP Positive COVID-19 Tests as of %s<br>%s (7d Moving Average)' % (date, sub),
+             color_discrete_sequence=px.colors.qualitative.Dark24,
+             labels={'P':'Positive Tests', 'jour':'Date'},)
+fig.write_html(file='./build/dep7.html')
+
 # All of France
 data = pandas.read_csv('https://www.data.gouv.fr/fr/datasets/r/dd0de5d9-b5a5-4503-930a-7b08dc0adc7c', sep=';')
 everyone = data[data['cl_age90'] == 0]
@@ -67,3 +89,10 @@ fig = px.bar(everyone, x='jour', y='P', template='ggplot2',
              title='France National SI-DEP Positive COVID-19 Tests as of %s<br>%s' % (date, sub),
              labels={'P':'Positive Tests', 'jour':'Date'},)
 fig.write_html(file='./build/national.html')
+
+everyone['P_7'] = everyone.rolling(7).mean()['P']
+
+fig = px.bar(everyone.dropna(), x='jour', y='P_7', template='ggplot2',
+             title='France National SI-DEP Positive COVID-19 Tests as of %s<br>%s (7d Moving Average)' % (date, sub),
+             labels={'P':'Positive Tests', 'jour':'Date'},)
+fig.write_html(file='./build/national7.html')
